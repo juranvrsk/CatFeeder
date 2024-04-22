9c2172930069a4e691c13f5e2805ab06bb525450
yurij <pim1289@yandex.ru>
2024-04-22 22:29:17 +0300

    Renewed the time class. Added pseudo-RTC functions

--- CFTime.h
+++ CFTime.h
@@ -1,50 +1,108 @@
+//Datetime-like and pseudo-RTC class
 class CFTime
 {
+    public:
+    struct TimeStamp
+    {
+        byte Hour;
+        byte Minute;
+        byte Second;
+    };
+    
+
     private:
-    int _hour;
-    int _minute;
+    TimeStamp _time;
+    byte _lastSeconds;
 
     public:
 
     CFTime()
     {
-        _hour = 0;
-        _minute = 0;
+        _time = {0, 0, 0};
+        _lastSeconds = 0;
     }    
 
-    CFTime(int hour, int minute)
+    CFTime(byte hour, byte minute, byte second)
     {
-        _hour = hour;
-        _minute = minute;
+        _time = {hour, minute, second};
+        _lastSeconds = 0;
     }
 
-    void SetTime(int hour, int minute)
+    CFTime(TimeStamp time)
     {
-        _hour = hour;
-        _minute = minute;
+        _time = time;
+    }
+
+    void SetTime(byte hour, byte minute, byte second)
+    {
+        _time = {hour, minute, second};
+    }
+
+    void SetTime(TimeStamp time)
+    {
+        _time = time;
     }
 
     int Hour()
     {
-        return _hour;
+        return _time.Hour;
     }
 
     int Minute()
     {
-        return _minute;
+        return _time.Minute;
+    }
+
+    int Second()
+    {
+        return _time.Second;
+    }
+
+    TimeStamp Time()
+    {
+        return _time;
     }
 
     String ToString()
     {
-        String result = String(_hour);
+        String result = String(_time.Hour);
         result += ":";
-        result += String(_minute);
+        result += String(_time.Minute);
+        result += ":";
+        result += String(_time.Second);
         return result;
     }
 
-    bool IsTime(int hour, int minute)//Comparing and logical summing
+    bool IsTime(byte hour, byte minute, byte second)//Comparing and logical summing
     {        
-        return (_hour==hour)&&(_minute==minute);
+        return (_time.Hour == hour)&&(_time.Minute == minute)&&(_time.Second == second);
+    }
+
+    bool IsTime(TimeStamp time)
+    {
+        return (_time.Hour == time.Hour)&&(_time.Minute == time.Minute)&&(_time.Second==time.Second);
+    }
+
+    void Tick()
+    {
+        //From UC boot we start millis, every cycle it's grow in limit of one minute
+        byte nowSeconds = millis()/1000;
+        _time.Second = nowSeconds - _lastSeconds;
+        //Replace to math functions?
+        if(_time.Second == 60) // Where the one minute is reached we fill the last seconds for count new
+        {
+            _lastSeconds = nowSeconds;
+            _time.Minute++;
+        }
+        if(_time.Minute == 60)//Passing the one hour limit is reset the minute count
+        {
+            _time.Minute = 0;
+            _time.Hour++;
+        }
+        if(_time.Hour == 24) //One day 
+        {
+            _time.Hour = 0;
+        }
     }
 
 };
